182336f54fdf03279809fcab90487051a76c7c2d
Zheleznikova Ekaterina <61594628+kZhel@users.noreply.github.com>
2022-09-19 23:18:49 +0300

    init

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
-
+<!--zhopa-->
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="#" class="nav-link px-2 text-secondary">Главная</a></li>
                 <?php
