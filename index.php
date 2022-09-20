11dc83224690cb783bc73359cbe321899e41ccd7
Екатерина Виноградова <kanadi2909@gmail.com>
2022-09-20 21:41:42 +0300

    test

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <header class="p-3 text-bg-dark">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-
+<!---->
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="#" class="nav-link px-2 text-white">Главная</a></li>
                 <?php
