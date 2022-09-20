a733939a21f5b3d971bf54fab27a97f7378a0fda
Zheleznikova Ekaterina <61594628+kZhel@users.noreply.github.com>
2022-09-20 20:16:58 +0300

    init

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-secondary">Главная</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Главная</a></li>
                 <?php
                 if (isset($_COOKIE['role']) && $_COOKIE['role'] != 2) {
                     echo '<li><a href="pages/my_page.php" class="nav-link px-2 text-white">Личный кабинет</a></li>';
